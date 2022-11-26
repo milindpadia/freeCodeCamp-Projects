e02df1bcb1b66bf2e59c991b9b9f684eca4d9c80
Milind Padia <padiamilind50@gmail.com>
2022-11-26 10:46:49 +0530

    Updated time_calculator.py
    Added code to handle the situation if hours is more than 24 and print the future day with respect to number of days passed.

--- Scientific Computing with Python/time_calculator.py
+++ Scientific Computing with Python/time_calculator.py
@@ -1,4 +1,6 @@
 def add_time(start_time, duration_time, day=None):
+    days_of_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    # days_of_week = {1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday', 7:'Sunday'}
     # function to get hours from time
     def get_hours(time):
         time = time.split()[0]
@@ -25,33 +27,76 @@ def add_time(start_time, duration_time, day=None):
     final_minutes = start_time_minutes + duration_time_minutes
     final_hours = start_time_hours + duration_time_hours
 
-    if am_or_pm == "am":
+    no_of_days_later = 0
+    if final_hours > 24:
+        while final_hours > 24:
+            final_hours -= 24
+            no_of_days_later += 1
         if final_hours > 12:
-            am_or_pm = "PM"
             final_hours -= 12
-        else:
-            am_or_pm = "AM"
-        if final_minutes > 60:
-            final_minutes -= 60
-            final_minutes = "0"+str(final_minutes)
-            final_hours += 1
-            if final_hours >= 12:
-                am_or_pm = "PM"
-    else:
-        if final_hours > 12:
-            am_or_pm = "AM"
-            final_hours -= 12
-        else:
-            am_or_pm = "PM"
-        if final_minutes > 60:
-            final_minutes -= 60
-            final_minutes = "0"+str(final_minutes)
-            final_hours += 1
-            if final_hours >= 12:
+            no_of_days_later += 1
+            if am_or_pm == 'pm':
                 am_or_pm = "AM"
+            else:
+                am_or_pm = 'PM'
+        if final_minutes > 60:
+            final_minutes -= 60
+            final_minutes = "0"+str(final_minutes)
+            final_hours += 1
+            no_of_days_later += 1
+            if am_or_pm == 'pm':
+                am_or_pm = "AM"
+            else:
+                am_or_pm = 'PM'
+            
+            
+    else:
+        if am_or_pm == "am":
+            if final_hours > 12:
+                am_or_pm = "PM"
+                final_hours -= 12
+            else:
+                am_or_pm = "AM"
+            if final_minutes > 60:
+                final_minutes -= 60
+                final_minutes = "0"+str(final_minutes)
+                final_hours += 1
+                if final_hours >= 12:
+                    am_or_pm = "PM"
+        else:
+            if final_hours > 12:
+                am_or_pm = "AM"
+                final_hours -= 12
+            else:
+                am_or_pm = "PM"
+            if final_minutes > 60:
+                final_minutes -= 60
+                final_minutes = "0"+str(final_minutes)
+                final_hours += 1
+                if final_hours >= 12:
+                    am_or_pm = "AM"
     
     # return "{}:{} {}".format(final_hours, final_minutes, am_or_pm)
-    print("{}:{} {}".format(final_hours, final_minutes, am_or_pm))
+    print("{}:{} {},".format(final_hours, final_minutes, am_or_pm), end=" ")
 
-add_time("5:23 PM", "0:20")
+    # to print the day of week
+    if day == None:
+        pass
+    else:
+        index = days_of_week.index(day.title())
+        future_day = index + no_of_days_later
+        if future_day > 7:
+            while future_day > 7:
+                future_day -= 7
+            print(days_of_week[future_day], end=" ")
+        else:
+            print(days_of_week[index + no_of_days_later], end=" ")
+
+    # to print (no. of days later)
+    if no_of_days_later == 1:
+        print("(next day)")
+    else:
+        print("({} days later)".format(no_of_days_later), end=" ")
+
+add_time("11:43 PM", "24:20", "tueSday")
 
